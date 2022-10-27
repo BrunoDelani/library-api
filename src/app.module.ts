97d27e837c95f293ff6a861c8f39f7fd8a493dd4
Bruno Delani Cirino dos Santos <bruno.delani@compasso.com.br>
2022-10-27 15:49:11 -0300

    add established connection to the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CONNECTION_DB, CONNECTION_HOST, CONNECTION_PASSWORD, CONNECTION_PORT, CONNECTION_USERNAME } from './config/environment.config';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: "mysql",
+    host: CONNECTION_HOST,
+    port: CONNECTION_PORT,
+    username: CONNECTION_USERNAME,
+    password: CONNECTION_PASSWORD,
+    database: CONNECTION_DB,
+    entities: [],
+    synchronize: false,
+    autoLoadEntities: true,
+    migrationsRun: true,
+    migrations: ['./dist/core/typeorm/migrations/*.{js,ts}'],
+  })],
   controllers: [],
   providers: [],
 })
