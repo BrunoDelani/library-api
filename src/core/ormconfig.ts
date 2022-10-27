6de185ff4746098af1c8ce4ddea9094dad9b9714
Bruno Delani Cirino dos Santos <bruno.delani@compasso.com.br>
2022-10-27 16:06:03 -0300

    add ormconfig file

---
+++ src/core/ormconfig.ts
@@ -0,0 +1,25 @@
+import { CONNECTION_DB, CONNECTION_HOST, CONNECTION_PASSWORD, CONNECTION_PORT, CONNECTION_USERNAME } from "src/config/environment.config";
+import { DataSource } from "typeorm";
+
+const dataSource = new DataSource({
+    type: "mysql",
+    host: CONNECTION_HOST,
+    port: CONNECTION_PORT,
+    username: CONNECTION_USERNAME,
+    password: CONNECTION_PASSWORD,
+    database: CONNECTION_DB,
+    entities: ['./dist/core/typeorm/entity/*.entity.{js,ts}'],
+    synchronize: true,
+    migrationsRun: true,
+    migrations: ['./dist/core/typeorm/migrations/*.{js,ts}'],
+})
+
+dataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!");
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err);
+    });
+
+export default dataSource;
