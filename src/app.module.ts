0bac6dfa320d6441636dc85ba12fb726baa870af
Bruno Delani Cirino dos Santos <bruno.delani@compasso.com.br>
2022-10-28 11:21:05 -0300

    add start module category with nest create new resource in comand line

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CONNECTION_DB, CONNECTION_HOST, CONNECTION_PASSWORD, CONNECTION_PORT, CONNECTION_USERNAME } from './config/environment.config';
+import { Book } from './core/typeorm/entities/book.entity';
+import { Category } from './core/typeorm/entities/category.entity';
+import { CategoriesModule } from './modules/categories/categories.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
@@ -10,12 +13,12 @@ import { CONNECTION_DB, CONNECTION_HOST, CONNECTION_PASSWORD, CONNECTION_PORT, C
     username: CONNECTION_USERNAME,
     password: CONNECTION_PASSWORD,
     database: CONNECTION_DB,
-    entities: [],
+    entities: [Category, Book],
     synchronize: false,
     autoLoadEntities: true,
     migrationsRun: true,
     migrations: ['./dist/core/typeorm/migrations/*.{js,ts}'],
-  })],
+  }), CategoriesModule],
   controllers: [],
   providers: [],
 })
