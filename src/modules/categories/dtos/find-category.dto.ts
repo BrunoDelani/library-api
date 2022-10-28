b076300f79409d770d0e372db2051ceaf9dd3f7f
Bruno Delani Cirino dos Santos <bruno.delani@compasso.com.br>
2022-10-28 13:32:16 -0300

    refactor rename class findCategoryDto

--- src/modules/categories/dtos/find-category.dto.ts
+++ src/modules/categories/dtos/find-category.dto.ts
@@ -2,7 +2,7 @@ import { PartialType } from '@nestjs/mapped-types';
 import { IsOptional } from 'class-validator';
 import { CreateCategoryDto } from './create-category.dto';
 
-export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {
+export class FindCategoryDto extends PartialType(CreateCategoryDto) {
     @IsOptional()
     name: string;
 }
