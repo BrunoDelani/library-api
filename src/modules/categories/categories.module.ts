69a753b4029e2b28975352e09370ef34b5b228e5
Bruno Delani Cirino dos Santos <bruno.delani@compasso.com.br>
2022-10-28 13:47:00 -0300

    add category access routes are defined and default

--- src/modules/categories/categories.module.ts
+++ src/modules/categories/categories.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { CategoriesService } from './services/categories.service';
 import { CategoriesController } from './controllers/categories.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Category } from 'src/core/typeorm/entities/category.entity';
+import { CategoryTypeOrmRepository } from './data/categories.typeorm.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Category])],
   controllers: [CategoriesController],
-  providers: [CategoriesService]
+  providers: [CategoriesService,
+    {
+      provide: 'categoryRepository',
+      useClass: CategoryTypeOrmRepository
+    },
+  ],
 })
-export class CategoriesModule {}
+export class CategoriesModule { }
