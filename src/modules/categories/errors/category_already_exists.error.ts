0e278a9443a34ad554d20ebe6f9f65e944998095
Bruno Delani Cirino dos Santos <bruno.delani@compasso.com.br>
2022-10-28 16:34:20 -0300

    refactor handling errors in one file for categories

--- src/modules/categories/errors/category_already_exists.error.ts
+++
@@ -1,8 +0,0 @@
-export class CategoryErrorAlreadyExists extends Error {
-    private readonly statusCode: number;
-    constructor() {
-        super()
-        this.message = 'Category already exists.';
-        this.statusCode = 401;
-    }
-}
