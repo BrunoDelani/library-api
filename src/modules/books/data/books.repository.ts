071897320ebd82a2e2dc0073bfc5b56869863304
BrunoDelani <br.delani@gmail.com>
2022-10-31 15:04:30 -0300

    add repository book with default methods

---
+++ src/modules/books/data/books.repository.ts
@@ -0,0 +1,13 @@
+import { BadRequestException } from "@nestjs/common";
+import { Book } from "src/core/typeorm/entities/book.entity";
+import { FindCategoryDto } from "src/modules/categories/dtos/find-category.dto";
+import { CreateBookDto } from "../dtos/create-book.dto";
+
+export interface BookRepository {
+    create(payload: CreateBookDto): Promise<Book>;
+    findAll(query: FindCategoryDto): Promise<Book[]>;
+    findOne(id: string): Promise<Book | null>;
+    findOneByName(name: string): Promise<Book | null>;
+    update(payload: Book): Promise<Book | BadRequestException>;
+    remove(id: string): Promise<void>;
+}
