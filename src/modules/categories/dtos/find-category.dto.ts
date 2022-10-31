a11413a8f5b57425c4a1801f28dc2dcb0144d390
Bruno Delani Cirino dos Santos <bruno.delani@compasso.com.br>
2022-10-31 10:49:33 -0300

    fix get all categories didn't allow query

--- src/modules/categories/dtos/find-category.dto.ts
+++ src/modules/categories/dtos/find-category.dto.ts
@@ -1,9 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { IsEmpty, IsOptional } from 'class-validator';
+import { IsOptional, MinLength } from 'class-validator';
 import { CreateCategoryDto } from './create-category.dto';
 
 export class FindCategoryDto extends PartialType(CreateCategoryDto) {
     @IsOptional()
-    @IsEmpty()
+    @MinLength(0, {})
     name?: string;
- }
+}
